b41d270b112989f185be6753423be1bd72cb88f0
Sergio Ruiz <sergio.rruvalcaba@alumnos.udg.mx>
2020-10-15 01:33:05 -0500

    prueba 01

--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var n int
+	var x int
+	var s []int // slice
+	var resultado int
+	fmt.Scan(&n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&x)
+		s = append(s, x)
+	}
+	for _, k := range s {
+		//fmt.Println(k)
+		resultado += k
+	}
+	fmt.Println(resultado)
 }
